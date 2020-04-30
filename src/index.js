d3483f08d7e0a6ce3ed7ca37e3eae82642363c68
RXBVK <52883785+RXBVK@users.noreply.github.com>
2020-04-30 18:02:18 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -64,6 +64,37 @@ export class App extends React.Component {
 init(sdk => {
   ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
 });
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: "",
+      top: "",
+      left: ""
+    }
+  }
+  handleCoords = (e) => {
+    this.setState({
+      top: `${Math.round(e.nativeEvent.offsetY * 100 / e.target.height)}%`,
+      left: `${Math.round(e.nativeEvent.offsetX * 100 / e.target.width)}%`
+    })
+  }
+  render() {
+    return (
+      <div className="point-editor-wrap">
+        <input type="file" accept="image/png, image/jpeg" id="image-input" value={this.state.value} onInput={(e) => { this.setState({ imgSrc: e.target.files[0].name }) }} />
+        <div id="image-container">
+          {this.state.imgSrc ? <img src={this.state.imgSrc} onClick={this.handleCoords} /> : "Choose an image"}
+        </div>
+        <TextInput type="text" width="medium" value={this.state.top || "Click on image to set Y."} disabled />
+        <TextInput type="text" width="medium" value={this.state.left || "Click on image to set X."} disabled />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
 /**
  * By default, iframe of the extension is fully reloaded on every save of a source file.
  * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
