d6891cca2acd04fb52bee5289feb3566981a0205
RXBVK <52883785+RXBVK@users.noreply.github.com>
2020-05-03 15:09:21 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
+import { TextInput, EntryCard } from '@contentful/forma-36-react-components';
 import { init, locations } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
@@ -16,13 +16,23 @@ export class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: props.sdk.field.getValue() || ''
+      value: props.sdk.field.getValue() || '',
+      imgSrc: "",
+      top: "",
+      left: "",
+      coords: props.sdk.field.getValue() || ''
     };
   }
 
   componentDidMount() {
     this.props.sdk.window.startAutoResizer();
 
+    this.getImage;
+
+    const image = this.props.sdk.entry.fields.image;
+    const locales = this.props.sdk.locales.available[0];
+    const imageID = image.getValue().sys.id;
+    const asset = this.props.sdk.space.getAsset(imageID).then((response) => this.setState({ imgSrc: response.fields.file[locales].url }));
     // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
     this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
   }
@@ -47,16 +57,23 @@ export class App extends React.Component {
     }
   };
 
+
+  handleCoords = (e) => {
+    this.setState({
+      top: `${Math.round(e.nativeEvent.offsetY * 100 / e.target.height)}%`,
+      left: `${Math.round(e.nativeEvent.offsetX * 100 / e.target.width)}%`
+    })
+    this.props.sdk.field.setValue(this.state.top + 'x' + this.state.left);
+  }
+
   render() {
     return (
-      <TextInput
-        width="large"
-        type="text"
-        id="my-field"
-        testId="my-field"
-        value={this.state.value}
-        onChange={this.onChange}
-      />
+      <div className="point-editor-wrap">
+        <div id="image-container">
+          {this.state.imgSrc ? <img src={this.state.imgSrc} onClick={this.handleCoords} /> : "Chosen image will be displayed here."}
+        </div>
+        <TextInput type="text" width="medium" value={this.props.sdk.field.getValue() || "Click on image to set coordinates."} disabled />
+      </div>
     );
   }
 }
@@ -64,37 +81,6 @@ export class App extends React.Component {
 init(sdk => {
   ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
 });
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imgSrc: "",
-      top: "",
-      left: ""
-    }
-  }
-  handleCoords = (e) => {
-    this.setState({
-      top: `${Math.round(e.nativeEvent.offsetY * 100 / e.target.height)}%`,
-      left: `${Math.round(e.nativeEvent.offsetX * 100 / e.target.width)}%`
-    })
-  }
-  render() {
-    return (
-      <div className="point-editor-wrap">
-        <input type="file" accept="image/png, image/jpeg" id="image-input" value={this.state.value} onInput={(e) => { this.setState({ imgSrc: e.target.files[0].name }) }} />
-        <div id="image-container">
-          {this.state.imgSrc ? <img src={this.state.imgSrc} onClick={this.handleCoords} /> : "Choose an image"}
-        </div>
-        <TextInput type="text" width="medium" value={this.state.top || "Click on image to set Y."} disabled />
-        <TextInput type="text" width="medium" value={this.state.left || "Click on image to set X."} disabled />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<Main />, document.getElementById("root"));
 /**
  * By default, iframe of the extension is fully reloaded on every save of a source file.
  * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
